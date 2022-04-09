96bb7e182ba15b25bb6ff356da5f48a4e05fe58f
Annu <annujolly17@gmail.com>
2022-04-09 12:36:23 +0530

    minor grammatical update

--- Main.py
+++ Main.py
@@ -11,9 +11,10 @@ import json
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SECRET_KEY'] = 'the random string'    
+app.config['SECRET_KEY'] = 'the random string'
 db = SQLAlchemy(app)
 ph = PasswordHasher()
+
 
 class User(db.Model):
 
@@ -45,10 +46,10 @@ class Transcation(db.Model):
 def getQuotePrice(symbol):
     base_url = 'https://financialmodelingprep.com/api/v3/stock/real-time-price/'
     content = urllib.urlopen(base_url + symbol).read()
-    print ('content is ', content)
+    print('content is ', content)
     json_content = json.loads(content)
-    print ('m is ', json_content['symbol'], ' price',
-           json_content['price'])
+    print('m is ', json_content['symbol'], ' price',
+          json_content['price'])
     if content:
         return json_content['price']
     else:
@@ -66,15 +67,14 @@ def login():
             email = request.form['email']
             password = request.form['password']
             data = User.query.filter_by(email=email).first()
-            
+
             if (data is not None) and ph.verify(data.password, password):
                 session['user'] = data.id
-                print (session['user'])
+                print(session['user'])
                 return redirect(url_for('home'))
             return render_template('incorrect_login.html')
         except:
             return render_template('incorrect_login.html')
-
 
 
 @app.route('/register/', methods=['GET', 'POST'])
@@ -95,7 +95,6 @@ def logout():
     return redirect(url_for('login'))
 
 
-
 #############################################################################
 
 @app.route('/home')
@@ -107,6 +106,7 @@ def home():
 
     return render_template('home.html', stock=stock, user=user_id, cash=u.cash_in_hand)
 
+
 @app.route('/show')
 def show():
     show_user = User.query.all()
@@ -117,7 +117,6 @@ def show():
 def stock():
     stock = Stock.query.all()
     return render_template('show.html', stock=stock)
-
 
 
 #############################################################################
@@ -135,11 +134,10 @@ def quote():
             quote = getQuotePrice(symbol)
             return render_template('quote.html', quote=quote)
         except:
-            return render_template('404.html', display_content='Invalid symbol. No quote available' )
+            return render_template('404.html', display_content='Invalid symbol. No quote available')
     else:
-    # GET method
+        # GET method
         return render_template('quote.html')
-
 
 
 #############################################################################
@@ -159,41 +157,42 @@ def buy():
             user_id = session['user']
             u = User.query.get(user_id)
             final_cash_in_hand = u.cash_in_hand - total_cash_spend
-            print ('final= ', final_cash_in_hand)
+            print('final= ', final_cash_in_hand)
             if final_cash_in_hand > 0:
                 u.cash_in_hand = final_cash_in_hand
-                s = Stock.query.filter_by(owner_id=user_id, name=symbol).first()
+                s = Stock.query.filter_by(
+                    owner_id=user_id, name=symbol).first()
                 if s:
-                    print ('final= ', final_cash_in_hand)
+                    print('final= ', final_cash_in_hand)
                     u.cash_in_hand = final_cash_in_hand
                     final_qty = s.qty + int(shares)
-                    print ('final qty', final_qty)
+                    print('final qty', final_qty)
                     s.qty = final_qty
                     db.session.commit()
                 else:
                     new_stock = Stock(name=symbol, qty=shares,
-                            owner_id=u.id, price=price)
-                    print ('new stock', new_stock)
+                                      owner_id=u.id, price=price)
+                    print('new stock', new_stock)
                     db.session.add(new_stock)
                     db.session.commit()
-                    print ('commited')
-                new_transcation = Transcation(type='Bought',name=symbol, qty=shares, owner_id=u.id)
+                    print('commited')
+                new_transcation = Transcation(
+                    type='Bought', name=symbol, qty=shares, owner_id=u.id)
                 db.session.add(new_transcation)
                 db.session.commit()
 
             else:
-                return render_template('404.html',display_content='Insufficient balance in the amount' )
+                return render_template('404.html', display_content='Insufficient balance in the amount')
 
             return redirect(url_for('home'))
         except:
             return render_template('404.html', display_content='Invalid symbol. No quote available')
     else:
-    # GET method
+        # GET method
         return render_template('buy.html')
 
 
-
-#############################################################################
+#################################################################################
 
 @app.route('/sell', methods=['GET', 'POST'])
 def sell():
@@ -202,7 +201,7 @@ def sell():
         shares = request.form['shares']
 
         if not request.form.get('symbol'):
-            return render_template('404.html',display_content='No symbol provided')
+            return render_template('404.html', display_content='No symbol provided')
 
         try:
             price = getQuotePrice(symbol)
@@ -210,30 +209,30 @@ def sell():
             user_id = session['user']
             u = User.query.get(user_id)
             s = Stock.query.filter_by(owner_id=user_id, name=symbol).first()
-            print (s.name, 'and symbol - ', symbol)
+            print(s.name, 'and symbol - ', symbol)
             if s.name == symbol and s.qty > 0:
                 final_cash_in_hand = u.cash_in_hand + total_cash_spend
-                print ('final= ', final_cash_in_hand)
+                print('final= ', final_cash_in_hand)
                 u.cash_in_hand = final_cash_in_hand
                 final_qty = s.qty - int(shares)
-                print ('final qty', final_qty)
+                print('final qty', final_qty)
                 s.qty = final_qty
-                new_transcation = Transcation(type='Sold', name=symbol, qty=shares, owner_id=u.id)
+                new_transcation = Transcation(
+                    type='Sold', name=symbol, qty=shares, owner_id=u.id)
                 db.session.add(new_transcation)
                 db.session.commit()
                 return redirect(url_for('home'))
             else:
-                return render_template('404.html',display_content='Insufficient shares to sell')
+                return render_template('404.html', display_content='Insufficient shares to sell')
 
         except:
             return render_template('404.html', display_content='123Invalid symbol. No quote available')
     else:
-    # GET method
+        # GET method
         return render_template('sell.html')
 
 
-
-#############################################################################
+###############################################################################
 
 @app.route('/history')
 def history():
@@ -242,11 +241,9 @@ def history():
     return render_template('history.html', transcation=transcation, user=user_id)
 
 
-
-#############################################################################
-
+################################################################################
 
 if __name__ == '__main__':
-    #Added secret key that the top app.secret_key = 'super secret key'
+    # Added secret key that the top app.secret_key = 'super secret key'
     db.create_all()
     app.run(debug=True)
